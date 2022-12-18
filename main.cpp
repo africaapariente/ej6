0252530cd3758603155dfb33b7dbfd28ea20f839
africa <africa.pariente.fora@opendeusto.es>
2022-12-18 15:25:33 +0100

    1812

--- main.cpp
+++ main.cpp
@@ -5,65 +5,70 @@
 
 #include "mbed.h"
 
-DigitalIn boton1(PC_13);
+enum estados {Parpadeo1000msec, Parpadeo500msec, Parpadeo250msec, EsperandoPulsar, EsperandoSoltar} estado;
+DigitalIn boton(PC_13);
 DigitalOut led1(LED1);
 Timer timer;
+int contador;
 
 void funcionParpadeo1000msec(){
-    led1=!led1;
-    wait_us(1000000);
-    if(boton==0){
-        timer.reset();
-        timer.start();
-        while(boton==0||timer.elapsedtime().count()<1000){
+        contador=0;
+        while(boton=1){
+        led1=!led1;
+        wait_us(3000000);
+        estado=EsperandoPulsar;
+}
+timer.reset();
+timer.start();
+while(boton=0||timer.read()<100){
 
-        }
-        timer.stop();
-        estado=Parpadeo500msec;
-        }
-
-    }
+}
+if(boton=0){
+    estado=EsperandoPulsar;
+}
+}
 
 void funcionParpadeo500msec(){
-    led1=!led1;
-    wait_us(500000);
-    if(boton==0){
-        timer.reset();
-        timer.start();
-        while(boton==0||timer.elapsedtime().count()<1000){
-
-        }
-        timer.stop();
-        estado=Parpadeo250msec;
-        }
+        led1=!led1;
+        wait_us(2000000);
 }
 void funcionParpadeo250msec(){
-    led1=!led1;
-    wait_us(250000);
-    if(boton==0){
-        timer.reset();
-        timer.start();
-        while(boton==0||timer.elapsedtime().count()<1000){
-
-        }
-        timer.stop();
-        estado=Parpadeo1000msec;
-        }
+        led1=!led1;
+        wait_us(1000000);
 }
+void funcionEsperandoPulsar{
+    contador=contador++;
+    if(boton==1){
+        while
+    }
+    timer.reset();
+    while(boton==1||timer.read()<100){
+    }
 
-int main()
-{   
+
+}
+void funcionEsperandoSoltar{
+
+}
+int main(){   
  while(1) {
-    estado=Parpadeo1000msec;
-    switch(estado) {
-            case Parpadeo1sec;
+     estado=Parpadeo1000msec;
+        switch(estado) {
+            case Parpadeo1000msec:
                 funcionParpadeo1000msec();
                 break;
-            case Parpadeo500msec;
+            case Parpadeo500msec:
                 funcionParpadeo500msec();
                 break;
-            case Parpadeo250msec;
+            case Parpadeo250msec:
                 funcionParpadeo250msec();
                 break;
+            case EsperandoPulsar:
+                funcionEsperandoPulsar();
+                break;
+            case EsperandoSoltar:
+                funcionEsperandoSoltar();
+                break;   
+        }
     }
 }
